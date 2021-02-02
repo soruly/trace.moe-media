4750c57e167db46486f74a48dac1bac6c511c053
soruly <soruly@gmail.com>
2021-02-02 22:49:51 +0800

    Use nodejs for secure link

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import "dotenv/config.js";
+import crypto from "crypto";
 import express from "express";
 import video from "./src/video.js";
 import image from "./src/image.js";
 
-const { SERVER_PORT, SERVER_ADDR } = process.env;
+const { SERVER_PORT, SERVER_ADDR, TRACE_MEDIA_SALT } = process.env;
 
 const app = express();
 // route for internal testing
@@ -20,6 +21,22 @@ app.get("/", (req, res) => {
   </script>
   `;
   res.send(html);
+});
+
+app.use(["/video/", "/image/"], async (req, res, next) => {
+  if (
+    req.query.token !==
+    crypto
+      .createHash("md5")
+      .update(`${req.query.t}${TRACE_MEDIA_SALT}`)
+      .digest("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=/g, "")
+  ) {
+    return res.sendStatus(403);
+  }
+  next();
 });
 
 app.get("/video/:anilistID/:filename", video);
