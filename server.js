cd4ffa075800ec8a795ccdae3da2d36bf8098523
soruly <soruly@gmail.com>
2021-02-03 00:14:45 +0800

    Add rate limit

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ import "dotenv/config.js";
 import express from "express";
 import video from "./src/video.js";
 import image from "./src/image.js";
+import rateLimit from "express-rate-limit";
 
 const { SERVER_PORT, SERVER_ADDR } = process.env;
 
 const app = express();
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    max: 30, // 30 requests per IP address (per node.js process)
+    windowMs: 60 * 1000, // per 1 minute
+  })
+);
 
 app.get("/video/:anilistID/:filename", video);
 
