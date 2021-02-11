ed28acd154e9ec9d49da1046833443fdeedd7fd9
soruly <soruly@gmail.com>
2021-02-11 16:15:39 +0800

    Add X-Content-Type-Options header

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.set("Referrer-Policy", "no-referrer");
+  res.set("X-Content-Type-Options", "nosniff");
   res.set(
     "Content-Security-Policy",
     [
